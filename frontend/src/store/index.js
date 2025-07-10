ad921aa706dad3becbd9dce8ebee10758ec2fc4e
cry-kae3 <cry.kae3@gmail.com>
2025-07-10 15:04:19 +0900

    Delete frontend directory

--- frontend/src/store/index.js
+++
@@ -1,1 +0,0 @@
-import { createStore } from 'vuex'; import auth from './modules/auth'; import staff from './modules/staff'; import store from './modules/store'; import shift from './modules/shift'; export default createStore({ modules: { auth, staff, store, shift } });
